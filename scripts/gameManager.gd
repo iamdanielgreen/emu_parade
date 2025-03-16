6ea3a007bbd345a4d592b3684b094968d206f3b2
iamdanielgreen <148546680+iamdanielgreen@users.noreply.github.com>
2025-03-16 18:00:45 +0000

    What did you even do this weekend? - 20250316_1756
    - Added physics layer to edge blocks in demo level.
    - Added Dialogic Files folders; completed timeline file from tutorial.
    - Added Quit Game functionality to ESC key.
    - Created GameManager.gd
    - Addition credits.
    - - Changed documents from "process/notes.txt" to process/credits.txt

---
+++ scripts/gameManager.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var paused = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	Dialogic.start("tutorial_timeline")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	#TODO: PAUSE MENU - Doesn't work now. Needs canvas layers and such.
+	#if Input.is_action_just_pressed("pause"):
+		#if paused == false:
+			#get_tree().paused = true
+		#elif paused == true:
+			#get_tree().paused = false
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func _on_dialogic_signal(argument: String):
+	if argument == "it's a signal":
+		print("A signal was fired via Dialogic")
