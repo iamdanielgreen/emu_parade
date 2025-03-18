ce9aa978eab7386f18dc2f8fe3e409be38a7c329
iamdanielgreen <148546680+iamdanielgreen@users.noreply.github.com>
2025-03-18 21:25:45 +0000

    Fixed Pause Menu - 20250318_2125
    - Set Pause Menu as CanvasLayer as opposed to ColorRect attached to Player camera.
    - Changed credits slide

--- scripts/menus/pause_menu.gd
+++ scripts/menus/pause_menu.gd
@@ -1,28 +1,37 @@
-extends ColorRect
+extends CanvasLayer
 
-@onready var pause_menu: ColorRect = $"."
 @onready var resume_button: Button = $VBoxContainer/resume_button
-@onready var main_menu_button: Button = $VBoxContainer/main_menu_button
+@onready var pause_menu: CanvasLayer = $"."
 
 var paused = false
+
+#TODO: Pretty sure this is redundant.
+#func _ready() -> void:
+	#self.hide()
 
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
+		#self.show()
+		#get_tree().paused = true
 		resume_button.grab_focus()
-
+		
 func pauseMenu():
 	if paused:
-		get_tree().paused = false
 		pause_menu.hide()
+		get_tree().paused = false
+		print("Game should be running now.")
 	else:
-		get_tree().paused = true
 		pause_menu.show()
+		get_tree().paused = true
+		print("Game is paused.")
 		
-		
+	paused = !paused #TODO: Someone needs to explain to you what this means.
+
+
 func _on_resume_button_pressed() -> void:
 	pauseMenu()
-	
+
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
